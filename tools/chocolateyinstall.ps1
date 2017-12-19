d13251d61ce60df036b1d200f331faba537282c3
Bob Tanner <tanner@real-time.com>
2017-12-19 14:21:51 -0600

    Fixes #4
    I misunderstood Join-Path, created $pkg32 and $pkg64 filenames then used
    Join-Path

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,10 +2,12 @@
 
 $clientRelease = "5.6.3"
 $clientBuild   = "1130"
+$pkg32         = 'FortiClientSetup_' + $clientRelease + '.' + $clientBuild + '.zip'
+$pkg64         = 'FortiClientSetup_' + $clientRelease + '.' + $clientBuild + '_x64.zip'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file          = Join-Path $toolsDir 'FortiClientSetup_' + $clientRelease + '.' + $clientBuild + '.zip'
+$file          = Join-Path $toolsDir $pkg32
 $checksum      = '11d9a6456fc34c8e0a239474ef9ea7b08fd7a1b1a89f766175285a892f3e1e65'
-$file64        = Join-Path $toolsDir 'FortiClientSetup_' + $clientRelease + '.' + $clientBuild + '_x64.zip'
+$file64        = Join-Path $toolsDir $pkg64
 $checksum64    = '28cb6d4298ce9f37bfb542bcc7a61d9cc3d43676206ac059cf51c146f7a3f0a3'
 
 $packageArgs = @{
