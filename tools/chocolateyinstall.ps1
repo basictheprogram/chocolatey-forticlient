14b87cc68245e4cf149a13aac1cf7ce10254f460
Bob Tanner <tanner@real-time.com>
2020-10-23 18:05:06 -0500

    New branch 6.4
    New release version forticlient-6.4.1.11519

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
-﻿$ErrorActionPreference = 'Stop'; 
+﻿$ErrorActionPreference = 'Stop';
 
 # Download client zip files from https://support.fortinet.com/Download/FirmwareImages.aspx
 #
-$clientRelease = "6.0.9"
-$clientBuild = "0277"
-$pkg32 = 'FortiClientSetup_' + $clientRelease + '.' + $clientBuild + '.zip'
-$pkg64 = 'FortiClientSetup_' + $clientRelease + '.' + $clientBuild + '_x64.zip'
+$clientRelease = "6.4.1"
+$clientBuild = "1519"
+$pkg32 = 'FortiClientSSOSetup_' + $clientRelease + '.' + $clientBuild + '.zip'
+$pkg64 = 'FortiClientSSOSetup_' + $clientRelease + '.' + $clientBuild + '_x64.zip'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $file = Join-Path $toolsDir $pkg32
-$checksum = '8c2c98de14d0a80fb42b249e3a51340b431a769812bf60af4b5dc220b57a410e'
+$checksum = '05565b06a799d76a383bcfb4f20befd062392a95f21c3d1189f7b20b40e94960'
 $file64 = Join-Path $toolsDir $pkg64
-$checksum64 = 'a76bce60e9bc938f16a593474de34ba79c1ce50768419299b7b8c8d1cbf7af4a'
+$checksum64 = 'bd9f238f34b1db9ffc7ca89747d2ed9d24eea2b982048915493da19cf9bbe2c2'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -18,14 +18,14 @@ $packageArgs = @{
   unzipLocation  = $toolsDir
   file64         = $file64
   checksum       = $checksum
-  checksumType   = 'sha256' 
+  checksumType   = 'sha256'
   checksum64     = $checksum64
   checksumType64 = 'sha256'
 }
 
 # https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
 #
-Install-ChocolateyZipPackage @packageArgs 
+Install-ChocolateyZipPackage @packageArgs
 
 # https://forum.fortinet.com/tm.aspx?m=150054
 #
@@ -34,10 +34,11 @@ Install-ChocolateyZipPackage @packageArgs
 #
 $installArgs = @{
   packageName    = $env:ChocolateyPackageName
-  file           = Join-Path $toolsDir 'FortiClient.msi'
+  file           = Join-Path $toolsDir 'FortiClientSSO.msi'
   fileType       = 'msi'
-  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'FortiClient*'
 }
 
 Install-ChocolateyInstallPackage @installArgs
